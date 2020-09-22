8dcc25ec35b936e836ce933520e5c25cdf78e9b6
dobrienUNCG <d_obrien@uncg.edu>
2020-09-22 16:04:17 -0400

    init

---
+++ BaseX/Dummy_Data.py
@@ -0,0 +1,3 @@
+
+#Get Commandline Arguments https://www.tutorialspoint.com/python/python_command_line_arguments.htm
+
---
+++ BaseX/scripts/src/tag.py
@@ -0,0 +1,4 @@
+class Tag:
+    def __init__(self, name, close_tag = False):
+        self.name = name
+        self.closing_tag = closing_tag
